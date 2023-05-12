880edb994cb0f2a7a5ef091c9831ce5a0c4a142f
Omarn8 <96694027+Omarn8@users.noreply.github.com>
2023-05-12 11:08:36 +0300

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,14 @@
+package coursework;
+import java.util.*;
+
+public class Q1 {
+	public static void main(String[] args) {
+	    HashSet<Character> items = new HashSet<Character>();
+	    items.add('V');
+	    items.add('B');
+	    items.add('F');
+	    items.add('F');
+	    items.add('M');
+	    System.out.println(items);
+	  }
+}
