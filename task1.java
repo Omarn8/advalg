62124231f40f9962959797999455aa0ec8eda6d4
Omarn8 <96694027+Omarn8@users.noreply.github.com>
2023-05-12 22:05:50 +0300

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,14 +1,19 @@
 package coursework;
 import java.util.*;
-
-public class Q1 {
-	public static void main(String[] args) {
-	    HashSet<Character> items = new HashSet<Character>();
-	    items.add('V');
-	    items.add('B');
-	    items.add('F');
-	    items.add('F');
-	    items.add('M');
-	    System.out.println(items);
-	  }
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();     
+        Set<Character> uniqueChars = new HashSet<>();
+        for (char c : str.toCharArray()) {
+            if(!uniqueChars.contains(c)) {
+                uniqueChars.add(c);
+            }
+        }     
+        for (char c : uniqueChars) {
+            System.out.print(c + " ");
+        }
+        scanner.close();
+    }
 }
